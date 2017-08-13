6b342184568ab389fe82ec026386eb1fb69355e8
Lalbahadur Bandaru <lrb@Lalbahadurs-MacBook-Pro.local>
2017-08-13 16:22:45 -0500

    updated Hello class to give start info

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@
 public class Hello {
 
 	public static void main(String[] args) {
+        System.out.println("Before the loop starts...");
 
 		for(int i = 0; i<=10; i++){
 			System.out.println("Hello World..."+i);
